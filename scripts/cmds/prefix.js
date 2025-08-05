b3e75f65eac884a2353c467e715835cc8ca7470b
𝅃ᴛꫝ֟፝ؖ۬ᴍɪᴍ⸙ <reportking69@outlook.com>
2025-08-05 18:16:23 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -15,8 +15,8 @@ onChat: async function({ event, message, getLang }) {
 if (event.body && event.body.toLowerCase() === "prefix") {
 return message.reply({
 body: `
-‣ 𝐆𝐥𝐨𝐛𝐚𝐥 𝐩𝐫𝐞𝐟𝐢𝐱:${globalPrefix}
-‣𝐘𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐩𝐫𝐞𝐟𝐢𝐱:${globalPrefix}
+‣ 𝐆𝐥𝐨𝐛𝐚𝐥 𝐩𝐫𝐞𝐟𝐢𝐱:$ {globalPrefix}
+‣𝐘𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐩𝐫𝐞𝐟𝐢𝐱:,
 
 ‣ 𝐀𝐝𝐦𝐢𝐧 
 ‣ 🎀ᴛꫝᴍɪᴍ🎀
