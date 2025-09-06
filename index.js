9676bae73b7f0c559e5c96bb7d2752ef37a0fc2f
𓆩𝐂.𝐄.𝐎⸙𝐓𝐀𝐌𝐈𝐌𓆪 <reportking69@outlook.com>
2025-09-06 15:19:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-// index.js
+/**
+ * @author NTKhang
+ * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
+ * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
+ * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
+ *
+ * English:
+ * ! Please do not change the below code, it is very important for the project.
+ * It is my motivation to maintain and develop the project for free.
+ * ! If you change it, you will be banned forever
+ * Thank you for using
+ *
+ * Vietnamese:
+ * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
+ * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
+ * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
+ * Cảm ơn bạn đã sử dụng
+ */
 
-const express = require("express");
-const app = express();
+const { spawn } = require("child_process");
+const log = require("./logger/log.js");
 
-// Use the PORT provided by the hosting platform, or 3000 for local development
-const PORT = process.env.PORT || 3000;
+function startProject() {
+	const child = spawn("node", ["Goat.js"], {
+		cwd: __dirname,
+		stdio: "inherit",
+		shell: true
+	});
 
-// Middleware
-app.use(express.json());
+	child.on("close", (code) => {
+		if (code == 2) {
+			log.info("Restarting Project...");
+			startProject();
+		}
+	});
+}
 
-// Default route
-app.get("/", (req, res) => {
-  res.send("Hello World! Your app is running successfully.");
-});
-
-// Example route
-app.get("/ping", (req, res) => {
-  res.send("Pong!");
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startProject();
