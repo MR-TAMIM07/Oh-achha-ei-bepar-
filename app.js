6ba8abae01c13a1d9a9e6ae0667b3405205ecc48
𓆩𝐂.𝐄.𝐎⸙𝐓𝐀𝐌𝐈𝐌𓆪 <reportking69@outlook.com>
2025-09-09 14:44:37 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.get("/", (req, res) => res.send("✅ Bot is Running 24/7!"));
+
+const PORT = process.env.PORT || 3000;
+const URL = "https://tamim-82bf.onrender.com"; // tor render URL
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+// Self ping every 1 minute
+setInterval(() => {
+  axios.get(URL).catch(() => {});
+}, 1 * 60 * 1000); // 1 min
